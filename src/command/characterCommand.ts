07b663b9d7d41dd079b989dbca4c411a20ef3ef2
tallua <talluay@gmail.com>
2021-03-25 01:08:53 +0900

    add content for character command

--- src/command/characterCommand.ts
+++ src/command/characterCommand.ts
@@ -4,6 +4,23 @@ import { GrabLostarkCharacterPage } from '../web_utils';
 
 import { JSDOM } from 'jsdom';
 
+interface CharacterStat {
+  expeditionLevel: string;
+  characterLevel: string;
+  itemLevel: string;
+};
+
+function extractCharacterStat(doc: Document): CharacterStat {
+  const expeditionLevel = doc.querySelector('.level-info__expedition').children[1].textContent;
+  const characterLevel = doc.querySelector('.level-info__item').children[1].textContent;
+  const itemLevel = doc.querySelector('.level-info2__expedition').children[1].textContent;
+
+  return {
+    expeditionLevel,
+    characterLevel,
+    itemLevel
+  };
+}
 @onMessage('!character')
 @onMessage('!캐릭터')
 export default class characterCommand implements MessageCommand {
@@ -17,9 +34,9 @@ export default class characterCommand implements MessageCommand {
     await GrabLostarkCharacterPage(params[1])
       .then((data) => {
         const dom = new JSDOM(data);
-        const elem = dom.window.document.querySelector('.level-info__expedition');
+        const stat = extractCharacterStat(dom.window.document);
 
-        message.channel.send(`${elem.textContent}`);
+        message.channel.send(`\`\`\`이름: ${params[1]}\n캐릭터: ${stat.characterLevel}\n아이템: ${stat.itemLevel}\n원정대: ${stat.expeditionLevel}\`\`\``);
       }).catch((e) => {
         message.channel.send(`failed to get data of ${params[1]}`);
         console.log(e);
